fbf3129bd62fdb30984de211c1078d747f5b9657
Adarsh Senghani <47920913+Adarsh077@users.noreply.github.com>
2024-11-02 21:35:46 +0530

    feat: added configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,85 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Asia/Kolkata";
+
+  i18n.defaultLocale = "en_IN";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_IN";
+    LC_IDENTIFICATION = "en_IN";
+    LC_MEASUREMENT = "en_IN";
+    LC_MONETARY = "en_IN";
+    LC_NAME = "en_IN";
+    LC_NUMERIC = "en_IN";
+    LC_PAPER = "en_IN";
+    LC_TELEPHONE = "en_IN";
+    LC_TIME = "en_IN";
+  };
+
+  services.xserver.enable = true;
+
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  services.printing.enable = true;
+
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  users.users.adarsh = {
+    isNormalUser = true;
+    description = "Adarsh Senghani";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    ];
+  };
+
+  programs.firefox.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    vim
+    curl
+    google-chrome
+    vscode
+    brave
+    git
+    mongodb-compass
+    discord
+    postman
+    docker
+    deja-dup
+    keepassxc
+    spotify
+    stripe-cli
+    nodejs
+    pnpm
+  ];
+
+  services.openssh.enable = true;
+  system.stateVersion = "24.05"; # Did you read the comment?
+}
